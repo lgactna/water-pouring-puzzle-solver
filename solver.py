c6d5ba37af9600e24ae40603de0743260a0c9834
lgactna <48928674+lgactna@users.noreply.github.com>
2020-08-13 16:52:46 -0700

    make resolve_current

--- solver.py
+++ solver.py
@@ -12,76 +12,40 @@ Kill conditions:
 - If a previous state (set at global level in the format [[<1>,<2>],...] is reached after performing an action, destroy that branch
 - If either is the desired fill level, return that branch'''
 
+def resolve_current(action_id, current_1, size_1, current_2, size_2, target, already_tried, path):
+    if target in (current_1, current_2):
+        path.append([action_id, current_1, current_2, len(path)])
+        return path
+    else:
+        if [current_1, current_2] in already_tried:
+            print(already_tried)
+            print(f'killing {current_1}/{size_1}, {current_2}/{size_2} with a path length of {len(path)-1}')
+            return None
+        else:
+            already_tried.append([current_1, current_2])
+            path.append([action_id, current_1, current_2, len(path)])
+            result = branch(current_1, size_1, current_2, size_2, target, already_tried, path)
+            if result:
+                return result
+
 def empty_1(current_1, size_1, current_2, size_2, target, already_tried, path):
     current_1 = 0
-    path.append([0, current_1, current_2, len(path)])
     #print(f'empty 1:{current_1}/{size_1}, {current_2}/{size_2}')
     #in theory there is no way you should ever reach a target by emptying a container
     #but i guess it's here anyways
-    if target in (current_1, current_2):
-        return path
-    else:
-        if [current_1, current_2] in already_tried:
-            print(already_tried)
-            print(f'killing {current_1}/{size_1}, {current_2}/{size_2} with a path length of {len(path)-1}')
-            del path[-1]
-            return None
-        else:
-            already_tried.append([current_1, current_2])
-            result = branch(current_1, size_1, current_2, size_2, target, already_tried, path)
-            if result:
-                return result
+    return resolve_current(1, current_1, size_1, current_2, size_2, target, already_tried, path)
 def empty_2(current_1, size_1, current_2, size_2, target, already_tried, path):
     current_2 = 0
-    path.append([1, current_1, current_2, len(path)])
     #print(f'empty 2:{current_1}/{size_1}, {current_2}/{size_2}')
-    if target in (current_1, current_2):
-        return path
-    else:
-        if [current_1, current_2] in already_tried:
-            print(already_tried)
-            print(f'killing {current_1}/{size_1}, {current_2}/{size_2} with a path length of {len(path)-1}')
-            del path[-1]
-            return None
-        else:
-            already_tried.append([current_1, current_2])
-            result = branch(current_1, size_1, current_2, size_2, target, already_tried, path)
-            if result:
-                return result
+    return resolve_current(1, current_1, size_1, current_2, size_2, target, already_tried, path)
 def fill_1(current_1, size_1, current_2, size_2, target, already_tried, path):
     current_1 = size_1
-    path.append([2, current_1, current_2, len(path)])
     #print(f'fill 1:{current_1}/{size_1}, {current_2}/{size_2}')
-    if target in (current_1, current_2):
-        return path
-    else:
-        if [current_1, current_2] in already_tried:
-            print(already_tried)
-            print(f'killing {current_1}/{size_1}, {current_2}/{size_2} with a path length of {len(path)-1}')
-            del path[-1]
-            return None
-        else:
-            already_tried.append([current_1, current_2])
-            result = branch(current_1, size_1, current_2, size_2, target, already_tried, path)
-            if result:
-                return result
+    return resolve_current(2, current_1, size_1, current_2, size_2, target, already_tried, path)
 def fill_2(current_1, size_1, current_2, size_2, target, already_tried, path):
     current_2 = size_2
-    path.append([3, current_1, current_2, len(path)])
     #print(f'fill 2:{current_1}/{size_1}, {current_2}/{size_2}')
-    if target in (current_1, current_2):
-        return path
-    else:
-        if [current_1, current_2] in already_tried:
-            print(already_tried)
-            print(f'killing {current_1}/{size_1}, {current_2}/{size_2} with a path length of {len(path)-1}')
-            del path[-1]
-            return None
-        else:
-            already_tried.append([current_1, current_2])
-            result = branch(current_1, size_1, current_2, size_2, target, already_tried, path)
-            if result:
-                return result
+    return resolve_current(3, current_1, size_1, current_2, size_2, target, already_tried, path)
 def fill_1_from_2(current_1, size_1, current_2, size_2, target, already_tried, path):
     #print(f'1 from 2 start:{current_1}/{size_1}, {current_2}/{size_2}')
     if size_1 - current_1 > current_2:
@@ -90,22 +54,9 @@ def fill_1_from_2(current_1, size_1, current_2, size_2, target, already_tried, p
     else:
         current_2 -= size_1 - current_1
         current_1 = size_1
-    path.append([4, current_1, current_2, len(path)])
     #print(f'1 from 2 end:{current_1}/{size_1}, {current_2}/{size_2}')
     #print(f'1 from 2:{current_1}/{size_1}, {current_2}/{size_2}')
-    if target in (current_1, current_2):
-        return path
-    else:
-        if [current_1, current_2] in already_tried:
-            print(already_tried)
-            print(f'killing {current_1}/{size_1}, {current_2}/{size_2} with a path length of {len(path)-1}')
-            del path[-1]
-            return None
-        else:
-            already_tried.append([current_1, current_2])
-            result = branch(current_1, size_1, current_2, size_2, target, already_tried, path)
-            if result:
-                return result
+    return resolve_current(4, current_1, size_1, current_2, size_2, target, already_tried, path)
 def fill_2_from_1(current_1, size_1, current_2, size_2, target, already_tried, path):
     if size_2 - current_2 > current_1:
         current_2 += current_1
@@ -113,21 +64,8 @@ def fill_2_from_1(current_1, size_1, current_2, size_2, target, already_tried, p
     else:
         current_1 -= size_2 - current_2
         current_2 = size_2
-    path.append([5, current_1, current_2, len(path)])
     #print(f'2 from 1:{current_1}/{size_1}, {current_2}/{size_2}')
-    if target in (current_1, current_2):
-        return path
-    else:
-        if [current_1, current_2] in already_tried:
-            print(already_tried)
-            print(f'killing {current_1}/{size_1}, {current_2}/{size_2} with a path length of {len(path)-1}')
-            del path[-1]
-            return None
-        else:
-            already_tried.append([current_1, current_2])
-            result = branch(current_1, size_1, current_2, size_2, target, already_tried, path)
-            if result:
-                return result
+    return resolve_current(5, current_1, size_1, current_2, size_2, target, already_tried, path)
 def branch(current_1, size_1, current_2, size_2, target, already_tried, path):
     #print(f'{current_1}/{size_1}, {current_2}/{size_2}')
     a = empty_1(current_1, size_1, current_2, size_2, target, already_tried, path)
